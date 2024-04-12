f5610c57b85a7dabd879b8f92b1c88cc1a80525a
ajjget <ajjget@gmail.com>
2024-04-12 00:36:55 -0600

    Created about.js

--- public/general.js
+++ public/general.js
@@ -26,37 +26,5 @@ async function delay() {
     });
 }
   
-function displayFact() {
-    const apiKey = '6e3ef13a0bmsh371c2c3d53ec18fp148385jsn1cce91e21da0';
-    const apiUrl = 'https://webknox-trivia-knowledge-facts-v1.p.rapidapi.com/trivia/search';
-
-    fetch(apiUrl + '?topic=teeth', {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': apiKey,
-            'X-RapidAPI-Host': 'webknox-trivia-knowledge-facts-v1.p.rapidapi.com'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        const containerEl = document.querySelector('#api_place');
-        const facts = data.trivia;
-        const factIndex = Math.floor(Math.random() * facts.length);
-        // console.log(facts.length)
-        // for (let i = 0; i < facts.length; i++) {
-        //     console.log(facts[i]);
-        // }
-        const randomFact = facts[factIndex];
-
-        containerEl.textContent = randomFact;
-        console.log(randomFact);
-    })
-    .catch(error => {
-        console.error('Error fetching trivia:', error);
-    });
-}
-
-  
-displayFact();
 loadUsername();
 websocketNotifications();
